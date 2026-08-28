350ed0d5323a1987229584641dc1360b2c2a1786
Roshan-R2004 <roshan.rohit16sep@gmail.com>
2026-08-28 11:36:05 +0530

    Delete js/booking-config.js

--- js/booking-config.js
+++
@@ -1,7 +0,0 @@
-/* GeoProcess Consulting — live booking endpoint */
-window.GEOPROCESS_BOOKING = {
-  apiUrl: "https://script.google.com/macros/s/AKfycbwcakUv1XPjz3HGfxyJHRsYDj7BA6jdV0vIUOBmnaYzwa6sRL_g-taxBOmvJIbLi0ON/exec"
-          
-          
-  
-};
